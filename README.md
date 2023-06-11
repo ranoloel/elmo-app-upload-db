d61700de4c1d3e033be7f3d2854c05a0039a4f88
elmo ranolo <elmoranolo@gmail.com>
2023-06-12 00:18:07 +0800

    Revised some files but not final

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# elmo-thesis
-elmo-thesis
+# django-image-upload
+
+### Instructions:
+- pip3 install django
+- pip3 install mysqlclient
+- pip3 install Pillow
+- update DB settings in settings.py
+- python3 manage.py migrate
---
+++ upload/forms.py
@@ -0,0 +1,9 @@
+from django import forms 
+from .models import *
+  
+class UploadImageForm(forms.ModelForm): 
+  
+    class Meta: 
+        model = UploadedImage 
+        fields = ['image', 'title']
+        #fields = ['image', 'title', 'coordinates'] 
